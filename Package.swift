1a2775f5fdf633a4e7cb194795cce2dba5e55739
zhangyun <zhangyun@top-stack.com>
2021-07-30 12:15:55 +0800

    udpate

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .target(
             name: "ZYVisionDetector",
             path: "Source",
-            exclude: ["Pods", "ZYVisionDetector", "Podfile", "sample.gif", "ZYVisionDetector.podspec"]
+            exclude: ["Pods", "ZYVisionDetector/", "Podfile", "sample.gif", "ZYVisionDetector.podspec"]
         )
     ]
 )
